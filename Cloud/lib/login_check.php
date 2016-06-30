e829803504c0dd4713ed26f5ccee26d72f4e7b92
Pietro Nompleggio <pietro.nompleggio@gmail.com>
2016-06-30 23:12:26 +0200

    Delete all the stuff

--- Cloud/lib/login_check.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once $_SERVER['DOCUMENT_ROOT'].'/lib/login_functions.php';
- 
-if (!isLogged())
-{
-header ("Location: login.php");
-exit();
-}
-?>
--- Cloud/nodejs_iot/test_socket.js
+++
@@ -1,6 +0,0 @@
-var io = require('./node_modules/socket.io').listen(5000);
-
-io.sockets.on('connection', function (socket) {
-	
-	console.log('a user connected');
-});
